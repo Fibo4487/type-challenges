77692a6f98ff42ecc942eea6fe57ca98534d530d
hayeong <fibonacci2246@gmail.com>
2022-11-26 21:50:34 +0900

    medium omit, readonly-2, deep-readonly solved

--- scripts/00008-medium-readonly-2.ts
+++ scripts/00008-medium-readonly-2.ts
@@ -29,8 +29,8 @@ interface Expected {
 }
 
 // ============= Your Code Here =============
-type MyReadonly2<T, K> = {
-  readonly [P in keyof T as P extends K ? P : P]: T[P];
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in keyof T]: T[P];
 } & {
-  [P in keyof T as P extends K ? P : never]: T[P];
+  [P in Exclude<keyof T, K>]: T[P];
 };
--- scripts/00009-medium-deep-readonly.ts
+++ scripts/00009-medium-deep-readonly.ts
@@ -1,56 +1,55 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
-type cases = [
-  Expect<Equal<DeepReadonly<X>, Expected>>,
-]
+type cases = [Expect<Equal<DeepReadonly<X>, Expected>>];
 
 type X = {
-  a: () => 22
-  b: string
+  a: () => 22;
+  b: string;
   c: {
-    d: boolean
+    d: boolean;
     e: {
       g: {
         h: {
-          i: true
-          j: 'string'
-        }
-        k: 'hello'
-      }
+          i: true;
+          j: "string";
+        };
+        k: "hello";
+      };
       l: [
-        'hi',
+        "hi",
         {
-          m: ['hey']
-        },
-      ]
-    }
-  }
-}
+          m: ["hey"];
+        }
+      ];
+    };
+  };
+};
 
 type Expected = {
-  readonly a: () => 22
-  readonly b: string
+  readonly a: () => 22;
+  readonly b: string;
   readonly c: {
-    readonly d: boolean
+    readonly d: boolean;
     readonly e: {
       readonly g: {
         readonly h: {
-          readonly i: true
-          readonly j: 'string'
-        }
-        readonly k: 'hello'
-      }
+          readonly i: true;
+          readonly j: "string";
+        };
+        readonly k: "hello";
+      };
       readonly l: readonly [
-        'hi',
+        "hi",
         {
-          readonly m: readonly ['hey']
-        },
-      ]
-    }
-  }
-}
-
+          readonly m: readonly ["hey"];
+        }
+      ];
+    };
+  };
+};
 
 // ============= Your Code Here =============
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: keyof T[K] extends never ? T[K] : DeepReadonly<T[K]>;
+};
